d97189c501bde4df0622f6837cbf3effdddaaaee
Jenna Deamer <jennadeamer@gmail.com>
2025-08-14 21:55:08 -0400

    Add mid, left and right to buildTree

--- BST.js
+++ BST.js
@@ -1,30 +1,42 @@
 const prettyPrint = (node, prefix = '', isLeft = true) => {
-  if (node === null) {
-    return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  }
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-  }
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
 };
 
 
 function Node(data) {
-  return {
-    data,
-    left: null,
-    right: null
-  };
+    return {
+        data,
+        left: null,
+        right: null
+    };
 }
 
 export function Tree(arr) {
-  let root = buildTree(arr);
+    let root = buildTree(arr);
+    let start = arr[0];
+    let end = arr[-1];
 
+    function buildTree(arr, start, end) {
+        if (start > end) return null 
 
-  function buildTree(arr) { }
+        let mid = start + Math.floor((end - start) / 2); // Middle of arr
+        let root = new Node(arr[mid]);
 
-  return root;
+        root.left = buildTree(arr, start, mid - 1);
+        root.right = buildTree(arr, mid + 1, end);
+
+        return root;
+
+    }
+
+    return root;
 }
--- driver.js
+++ driver.js
@@ -1,3 +1,5 @@
 import {Tree} from "./BST.js";
+let sampleData = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-const test = Tree();
+const test = Tree(sampleData);
+console.log(test)
