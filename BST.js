4e7b9a8e0fc67f50419ed3ff15704680356a7d74
Jenna-Deamer <jennadeamer@gmail.com>
2025-08-17 19:54:44 -0400

    Add insert function that takes a value and inserts it as a new leaf on the tree

--- BST.js
+++ BST.js
@@ -1,46 +1,48 @@
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-    if (node === null) {
-        return;
-    }
-    if (node.right !== null) {
-        prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-    }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-    if (node.left !== null) {
-        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-    }
-};
-
-
-function Node(data) {
-    return {
-        data,
-        left: null,
-        right: null
-    };
+export function Node(data) {
+  return {
+    data,
+    left: null,
+    right: null
+  };
 }
 
 export function Tree(arr) {
-    let root = buildTree(arr, 0, arr.length - 1);
+  let root = buildTree(arr, 0, arr.length - 1);
 
 
-    function buildTree(arr, start, end) {
-        if (start > end) return null
-        console.log(start)
-        console.log(end)
+  function buildTree(arr, start, end) {
+    if (start > end) return null
 
-        let mid = start + Math.floor((end - start) / 2); // Middle of arr
-        console.log("Mid " + mid)
-        let root = new Node(arr[mid]);
-        console.log(root)
+    let mid = start + Math.floor((end - start) / 2); // Middle of arr
+    let root = new Node(arr[mid]);
 
-        root.left = buildTree(arr, start, mid - 1);
-        root.right = buildTree(arr, mid + 1, end);
-        return root;
-
-    }
-    console.log(root)
-    console.log(prettyPrint(root))
-
+    root.left = buildTree(arr, start, mid - 1);
+    root.right = buildTree(arr, mid + 1, end);
     return root;
+
+  }
+  // console.log(root)
+  // console.log(prettyPrint(root))
+
+  function insert(value, currentNode = root) {
+    if (currentNode == null) {
+      console.log('creating node....')
+      root = new Node(value);
+      return root;
+    }
+    if (currentNode.data === value) {
+      console.log('Node exists in tree!')
+      return currentNode;
+    }
+    if (value < currentNode.data) {
+      currentNode.left = insert(value,currentNode.left);
+    } else {
+      currentNode.right = insert(value, currentNode.right);
+    }
+    return currentNode
+  }
+
+  return { root, insert };
+
+
 }
--- driver.js
+++ driver.js
@@ -1,5 +1,27 @@
 import {Tree} from "./BST.js";
+import {Node} from "./BST.js";
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  };
+
+  
 const sampleData = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const sortedArray = Array.from(new Set(sampleData.sort((a, b) => a-b)));
-const test = Tree(sortedArray);
-console.log(test)
+const tree = Tree(sortedArray);
+console.log(tree)
+
+tree.insert(30);
+tree.insert(120);
+
+console.log('=====Tree=====');
+console.log(prettyPrint(tree.root));
