84f4f41baffd8d079c9a8f94354c6a9ca37eb1a7
Jenna Deamer <jennadeamer@gmail.com>
2025-08-21 18:18:13 -0400

    Add final test commands in driver

--- driver.js
+++ driver.js
@@ -14,13 +14,11 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-const sampleData = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const sampleData = [72, 15, 98,36,2,3,5, 43, 6, 57, 81, 24, 33, 90, 12, 65, 49, 8, 37, 76, 58, 91, 3, 84, 19, 67, 29, 54, 40];
 const sortedArray = Array.from(new Set(sampleData.sort((a, b) => a - b)));
 const tree = Tree(sortedArray);
 
-tree.insert(2)
-tree.insert(22)
-tree.insert(44444)
+
 
 
 // tree.deleteAt(4);
@@ -28,18 +26,18 @@ tree.insert(44444)
 // console.log('Found')
 // console.log(result)
 
-// console.log("Level Order");
-// tree.levelOrderForEach((val) => console.log(val.data));
+console.log("Level Order");
+tree.levelOrderForEach((val) => console.log(val.data));
 
 
-// console.log("In Order");
-// tree.inOrderForEach((val) => console.log(val.data));
+console.log("In Order");
+tree.inOrderForEach((val) => console.log(val.data));
 
-// console.log("Pre Order");
-// tree.preOrderForEach((val) => console.log(val.data));
+console.log("Pre Order");
+tree.preOrderForEach((val) => console.log(val.data));
 
-// console.log("Post Order");
-// tree.postOrderForEach((val) => console.log(val.data));
+console.log("Post Order");
+tree.postOrderForEach((val) => console.log(val.data));
 
 // console.log('Depth')
 // let depth = tree.depth(6345)
@@ -48,10 +46,13 @@ tree.insert(44444)
 console.log('=====Tree=====');
 console.log(prettyPrint(tree.root));
 
-
+console.log(tree.isBalanced());
+tree.insert(2)
+tree.insert(22)
+tree.insert(44444)
+console.log(tree.isBalanced());
 let result =  tree.rebalance();
 console.log(tree.isBalanced());
-
 console.log(result)
 
     
